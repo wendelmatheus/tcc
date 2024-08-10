4ebaa79597eb96517ed1077ca60f47cdf0ead7a2
Wendel <wendeltel6a@gmail.com>
2024-08-10 02:46:26 -0300

    corrigido bug das páginas privadas aparecendo sem token

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,31 @@
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
+import { getAPIClient } from "./api/axios";
+
 export default function Dashboard() {
   return (
     <div className="bg-gray-700 h-screen flex items-center justify-center">
       <h1 className="text-white text-3xl">Dashboard</h1>
     </div>
-  );
+  )
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apiClient = getAPIClient(ctx);
+  const { ["sitededenuncias.token"]: token } = parseCookies(ctx);
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  await apiClient.get("/api/hello");
+
+  return {
+    props: {},
+  };
+};
